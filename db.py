18a81777116358125002beee56e845d1a05093a1
Rohan Alam <rohanalam402@gmail.com>
2026-05-07 14:58:37 +0530

    Refactor get_connection to use environment variables

--- db.py
+++ db.py
@@ -1,15 +1,4 @@
 import pymysql
-
-def get_connection():
-    return pymysql.connect(
-        host="trolley.proxy.rlwy.net",
-        port=19224,
-        user="root",
-        password="uuxudHDYCjcYFbKiVARCCuDWvbIuFtYD",
-        database="resume_builder",
-        cursorclass=pymysql.cursors.DictCursor
-    )
-import pymysql
 import os
 
 def get_connection():
@@ -21,5 +10,3 @@ def get_connection():
         database=os.getenv("resume_builder"),
         cursorclass=pymysql.cursors.DictCursor
     )
-
-# mysql://root:uuxudHDYCjcYFbKiVARCCuDWvbIuFtYD@trolley.proxy.rlwy.net:19224/resume_builder
